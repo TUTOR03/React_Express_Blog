2b608959c7982a9b821691236a7a7e9b3cd7f4bf
TUTOR03 <tutor03@MSILaptopArtem.localdomain>
2021-07-09 01:27:32 +0500

    added typescript for vscode optimization

--- server/server.ts
+++ server/server.ts
@@ -37,7 +37,7 @@ app.use(errorHandler)
 
 app.use((req, res) => {
   console.log('404 NOT FOUND')
-  return res.status(404).json({})
+  return res.status(404).json({ Hello: 'GOooooooo' })
 })
 
 mongoose.connect('mongodb://localhost/my_blog', {
